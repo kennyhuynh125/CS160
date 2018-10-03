197e86d8c4734657bf718e6cc4b7d2c4c93ab01a
Kenny Huynh <huynhkenny125@yahoo.com>
2018-10-02 21:27:42 -0700

    added payment/settings to header

--- client/src/components/Payment/index.js
+++ client/src/components/Payment/index.js
@@ -38,99 +38,111 @@ export default class Payment extends Component {
 
 
     render() {
+        const isLoggedIn = sessionStorage.getItem('isLoggedIn');
         return (
             <Container>
-                <h2>Payment Information</h2>
-                <h5>Saved Cards</h5>
-                {this.state.savedCards}
-                <p><Button onClick={this.toggleCard} style={{ marginBottom: '1rem' }}>Add New Card</Button>
-                    <Collapse isOpen={this.state.collapseCard}>
-                    <Card>
-                        <CardBody>
-                        <Form>
-                            <FormGroup onSubmit={this.addCard}>
-                                <Label for="Name">Name</Label>
-                                <Input type="text" name="name" id="name" onChange={this.handleNameChange} required />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="ccnum">Credit Card Type</Label>
-                                <p><Input type="select" name="cctype" id="cctype" onChange={this.handleCardTypeChange}>
-                                    <option value="visa">Visa</option>
-                                    <option value="mastercard">MasterCard</option>
-                                    <option value="amex">American Express</option>
-                                    <option value="discover">Discover</option>
-                                </Input></p>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="ccnum">Credit Card Number</Label>
-                                <Input type="password" name="ccnum" id="ccnum" onChange={this.handleCardNumChange} required />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="expdate">Expiration Date</Label>
-                                <p><Input type="select" name="exmonth" id="expmonth" onChange={this.handleExpMonthChange}>
-                                    <option value="1">January</option>
-                                    <option value="2">February</option>
-                                    <option value="3">March</option>
-                                    <option value="4">April</option>
-                                    <option value="5">May</option>
-                                    <option value="6">June</option>
-                                    <option value="7">July</option>
-                                    <option value="8">August</option>
-                                    <option value="9">September</option>
-                                    <option value="10">October</option>
-                                    <option value="11">November</option>
-                                    <option value="12">December</option>
-                                </Input>
-                                <Input type="select" name="expyear" id="expyear" onChange={this.handleExpYearChange}>
-                                    <option value="18">2018</option>
-                                    <option value="19">2019</option>
-                                    <option value="20">2020</option>
-                                    <option value="21">2021</option>
-                                    <option value="22">2022</option>
-                                    <option value="23">2023</option>
-                                    <option value="24">2024</option>
-                                </Input></p>
-                            </FormGroup>
-                            <Button>Submit</Button>
-                        </Form>
-                        </CardBody>
-                    </Card>
-                </Collapse></p>
-                <h5>Billing Address</h5>
-                {this.state.savedAddresses}
-                <p><Button onClick={this.toggleAddr} style={{ marginBottom: '1rem' }}>Add New Address</Button>
-                    <Collapse isOpen={this.state.collapseAddr}>
-                    <Card>
-                        <CardBody>
-                        <Form>
-                            <FormGroup onSubmit={this.addAddress}>
-                                <Label for="street">Street</Label>
-                                <Input type="text" name="street" id="street" onChange={this.handleStreetChange} required />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="aptno">Apartment Number</Label>
-                                <Input type="text" name="aptno" id="aptno" onChange={this.handleAptNumChange} />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="city">City</Label>
-                                <Input type="text" name="city" id="city" onChange={this.handleCityChange} />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="state">State</Label>
-                                <Input type="select" name="state" id="state" onChange={this.handleStateChange}>
-                                    <option value="CA">CA</option>
-                                    <option value="x">other states here</option>
-                                </Input>
-                                <Label for="country">Country</Label>
-                                <Input type="select" name="country" id="country" onChange={this.handleCountryChange}>
-                                    <option value="USA">USA</option>
-                                </Input>
-                            </FormGroup>
-                            <Button>Submit</Button>
-                        </Form>
-                        </CardBody>
-                    </Card>
-                </Collapse></p>
+                {
+                    isLoggedIn && (
+                        <div>
+                        <h2>Payment Information</h2>
+                            <h5>Saved Cards</h5>
+                            {this.state.savedCards}
+                            <p><Button onClick={this.toggleCard} style={{ marginBottom: '1rem' }}>Add New Card</Button>
+                                <Collapse isOpen={this.state.collapseCard}>
+                                <Card>
+                                    <CardBody>
+                                    <Form>
+                                        <FormGroup onSubmit={this.addCard}>
+                                            <Label for="Name">Name</Label>
+                                            <Input type="text" name="name" id="name" onChange={this.handleNameChange} required />
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Label for="ccnum">Credit Card Type</Label>
+                                            <p><Input type="select" name="cctype" id="cctype" onChange={this.handleCardTypeChange}>
+                                                <option value="visa">Visa</option>
+                                                <option value="mastercard">MasterCard</option>
+                                                <option value="amex">American Express</option>
+                                                <option value="discover">Discover</option>
+                                            </Input></p>
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Label for="ccnum">Credit Card Number</Label>
+                                            <Input type="password" name="ccnum" id="ccnum" onChange={this.handleCardNumChange} required />
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Label for="expdate">Expiration Date</Label>
+                                            <p><Input type="select" name="exmonth" id="expmonth" onChange={this.handleExpMonthChange}>
+                                                <option value="1">January</option>
+                                                <option value="2">February</option>
+                                                <option value="3">March</option>
+                                                <option value="4">April</option>
+                                                <option value="5">May</option>
+                                                <option value="6">June</option>
+                                                <option value="7">July</option>
+                                                <option value="8">August</option>
+                                                <option value="9">September</option>
+                                                <option value="10">October</option>
+                                                <option value="11">November</option>
+                                                <option value="12">December</option>
+                                            </Input>
+                                            <Input type="select" name="expyear" id="expyear" onChange={this.handleExpYearChange}>
+                                                <option value="18">2018</option>
+                                                <option value="19">2019</option>
+                                                <option value="20">2020</option>
+                                                <option value="21">2021</option>
+                                                <option value="22">2022</option>
+                                                <option value="23">2023</option>
+                                                <option value="24">2024</option>
+                                            </Input></p>
+                                        </FormGroup>
+                                        <Button>Submit</Button>
+                                    </Form>
+                                    </CardBody>
+                                </Card>
+                            </Collapse></p>
+                            <h5>Billing Address</h5>
+                            {this.state.savedAddresses}
+                            <p><Button onClick={this.toggleAddr} style={{ marginBottom: '1rem' }}>Add New Address</Button>
+                                <Collapse isOpen={this.state.collapseAddr}>
+                                <Card>
+                                    <CardBody>
+                                    <Form>
+                                        <FormGroup onSubmit={this.addAddress}>
+                                            <Label for="street">Street</Label>
+                                            <Input type="text" name="street" id="street" onChange={this.handleStreetChange} required />
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Label for="aptno">Apartment Number</Label>
+                                            <Input type="text" name="aptno" id="aptno" onChange={this.handleAptNumChange} />
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Label for="city">City</Label>
+                                            <Input type="text" name="city" id="city" onChange={this.handleCityChange} />
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Label for="state">State</Label>
+                                            <Input type="select" name="state" id="state" onChange={this.handleStateChange}>
+                                                <option value="CA">CA</option>
+                                                <option value="x">other states here</option>
+                                            </Input>
+                                            <Label for="country">Country</Label>
+                                            <Input type="select" name="country" id="country" onChange={this.handleCountryChange}>
+                                                <option value="USA">USA</option>
+                                            </Input>
+                                        </FormGroup>
+                                        <Button>Submit</Button>
+                                    </Form>
+                                    </CardBody>
+                                </Card>
+                            </Collapse></p>
+                        </div>
+                    )
+                }
+                {
+                    !isLoggedIn && (
+                        <div>You must be logged in to access this page!</div>
+                    )
+                }
             </Container>
         );
     }
--- client/src/components/Settings/index.js
+++ client/src/components/Settings/index.js
@@ -49,59 +49,71 @@ export default class Settings extends Component {
 
 
     render() {
+        const isLoggedIn = sessionStorage.getItem('isLoggedIn');
         return (
             <Container>
-                <h2>Account Settings</h2>
-                <h5>First Name:</h5>
-                <Editable
-                    name="First Name"
-                    dataType="text"
-                    mode="inline"
-                    value={this.state.firstName}
-                    onUpdate={this.handleFirstNameChange}
-                />
-                <h5>Last Name:</h5>
-                <Editable
-                    name="Last Name"
-                    dataType="text"
-                    mode="inline"
-                    value={this.state.lastName}
-                    onUpdate={this.handleLastNameChange}
-                />
-                <h5>E-mail:</h5>
-                <Editable
-                    name="e-mail"
-                    dataType="text"
-                    mode="inline"
-                    value={this.state.email}
-                    onUpdate={this.handleEmailChange}
-                    validate={(value) => {
-                        let validEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                        if(!validEmail.test(value)){
-                            return 'Must input a valid e-mail address';
-                        }
-                    }}
-                /> 
-                <h5>Phone Number:</h5>
-                <Editable
-                    name="phone"
-                    dataType="text"
-                    mode="inline"
-                    value={this.state.phone}
-                    onUpdate={this.handlePhoneChange}
-                    validate={(value) => {
-                        if(value % 1 !== 0 || value.length !== 10){
-                            return 'Must input a valid number (no spaces, include area code)';
-                        }
-                    }}
-                />
-                <h5>Allow Detours</h5>
-                <ButtonGroup>
-                    <Button color="success">On</Button>
-                    <Button color="link">Off</Button>
-                </ButtonGroup>
-                <h5><a href="#">Ride History</a></h5>
-                <h5><a href="#">Change Password</a></h5>
+                {
+                    isLoggedIn && (
+                        <div>
+                            <h2>Account Settings</h2>
+                            <h5>First Name:</h5>
+                            <Editable
+                                name="First Name"
+                                dataType="text"
+                                mode="inline"
+                                value={this.state.firstName}
+                                onUpdate={this.handleFirstNameChange}
+                            />
+                            <h5>Last Name:</h5>
+                            <Editable
+                                name="Last Name"
+                                dataType="text"
+                                mode="inline"
+                                value={this.state.lastName}
+                                onUpdate={this.handleLastNameChange}
+                            />
+                            <h5>E-mail:</h5>
+                            <Editable
+                                name="e-mail"
+                                dataType="text"
+                                mode="inline"
+                                value={this.state.email}
+                                onUpdate={this.handleEmailChange}
+                                validate={(value) => {
+                                    let validEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                                    if(!validEmail.test(value)){
+                                        return 'Must input a valid e-mail address';
+                                    }
+                                }}
+                            /> 
+                            <h5>Phone Number:</h5>
+                            <Editable
+                                name="phone"
+                                dataType="text"
+                                mode="inline"
+                                value={this.state.phone}
+                                onUpdate={this.handlePhoneChange}
+                                validate={(value) => {
+                                    if(value % 1 !== 0 || value.length !== 10){
+                                        return 'Must input a valid number (no spaces, include area code)';
+                                    }
+                                }}
+                            />
+                            <h5>Allow Detours</h5>
+                            <ButtonGroup>
+                                <Button color="success">On</Button>
+                                <Button color="link">Off</Button>
+                            </ButtonGroup>
+                            <h5><a href="/">Ride History</a></h5>
+                            <h5><a href="/">Change Password</a></h5>
+                        </div>
+                    )
+                }
+                {
+                    !isLoggedIn && (
+                        <div>You must be logged in to access this page!</div>
+                    )
+                }
             </Container>
         );
     }
