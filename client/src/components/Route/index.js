8a218e0f54f126b6cf831b9a6115bcfae70f0cd5
Jennifer Hsiung <neykun@gmail.com>
2018-10-16 18:47:35 -0700

    Location search, routing updates
    route map accepts current location and sets it as origin

--- client/src/components/Route/index.js
+++ client/src/components/Route/index.js
@@ -1,57 +1,85 @@
-/* global google */
 import React from 'react';
-const { compose, withProps, lifecycle } = require("recompose");
-const {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  DirectionsRenderer,
-  TrafficLayer,
-} = require("react-google-maps");
-
-const MapWithADirectionsRenderer = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDkOgKW9d-YxAC_8kar8EZ-aL90qazdSNc&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-    }),
-    withScriptjs, withGoogleMap,
-    lifecycle({
-        componentDidMount() {
-            const DirectionsService = new google.maps.DirectionsService();
-
-    DirectionsService.route({
-        origin: new google.maps.LatLng(37.3352, -121.8811),
-        destination: new google.maps.LatLng(37.7749, -122.4194),
-        travelMode: google.maps.TravelMode.DRIVING,
-    }, (result, status) => {
-        if (status === google.maps.DirectionsStatus.OK) {
-            this.setState({
-                directions: result
-            });
-        } else {
-            console.error(`error fetching directions ${result}`);
-        }
-    });
-    }
-    })
-)(props =>
-    <GoogleMap
-        defaultZoom={7}
-        defaultCenter={new google.maps.LatLng(37.3352, -121.8811)}
-    >
-    <TrafficLayer autoUpdate />
-    {props.directions && <DirectionsRenderer directions={props.directions} />}
-    </GoogleMap>
-);
-
+import { Button } from 'reactstrap';
+import axios from 'axios';
+import StandaloneSearchBox from './searchbox'
+import MapWithADirectionsRenderer from './directionsrenderer'
 
 export default class Map extends React.PureComponent {
+    constructor(props) {
+        super();
+        this.state = {
+            latitude: 0,
+            longitude: 0,
+            location: [],
+            destLatitude: 0,
+            destLongitude: 0,
+            destLocation: [],
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            const res = await fetch('/api/');
+            const users = await res.json();
+            this.setState({
+                users: users,
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    getGeoLocation = () => {
+        const geolocation = navigator.geolocation;
+        if (geolocation) {
+            geolocation.getCurrentPosition((position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                })
+            });
+        } else {
+            console.log('Not supported');
+        }
+    }
+
+    getLocation = () => {
+        const latitude = this.state.latitude;
+        const longitude = this.state.longitude;
+        axios.get(`/api/location/${latitude}/${longitude}`)
+        .then((response) => {
+            this.setState({ location: response.data });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    updateDestination = (lat, long) => {
+        this.setState({destLatitude: lat, destLongitude: long})
+    }
+
+
     render() {
         return (
         <div>
-            <MapWithADirectionsRenderer />
+            <StandaloneSearchBox onPlacesChanged={this.updateDestination(37.7749, -122.4194)}/>
+            <Button onClick={this.getGeoLocation}>Find Route</Button>
+                {
+                    this.state.latitude !== 0 && this.state.longitude !== 0 && 
+                    this.state.destLongitude !== 0 && this.state.destLongitude !== 0 && (
+                        <div>
+                            <p>Latitude: {this.state.latitude}</p>
+                            <p>Longitude: {this.state.longitude}</p>
+                            <MapWithADirectionsRenderer 
+                                latitude={this.state.latitude} 
+                                longitude={this.state.longitude} 
+                                destLatitude={this.state.destLatitude} 
+                                destLongitude={this.state.destLongitude} 
+                            />
+                        </div>
+                    )
+                }
         </div>
         )
     }
