23f8f25451467645dfcffa805a44ec5e3e7f75a3
Kenny Huynh <huynhkenny125@yahoo.com>
2018-11-15 23:31:08 -0800

    reordered order of urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
     path('api/', include('backend.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
