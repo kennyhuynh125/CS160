3c80164b9a334532b77e3c53f35ba0aa5eee8335
Kenny Huynh <huynhkenny125@yahoo.com>
2018-11-15 23:24:50 -0800

    fixed path for frontend

--- server/server/urls.py
+++ server/server/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('api/', include('backend.urls')),
 ]
