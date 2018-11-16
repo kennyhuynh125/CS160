334bb327ea3deecc246c2bc3d95372f229d9be93
Kenny Huynh <huynhkenny125@yahoo.com>
2018-11-15 22:45:21 -0800

    updated staticfiles change

--- server/server/settings.py
+++ server/server/settings.py
@@ -147,5 +147,5 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'client/build/static')
+    'client/build/static'
 ]
